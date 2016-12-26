68eb38ab96b4f42f7e5312c2b966e8a6a79a228b
root <root@e3dd451dfae0>
2016-12-26 04:54:33 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "VCSDemo",
+  "homePage" : "Main",
+  "name" : "VCSDemo",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
